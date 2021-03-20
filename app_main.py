151aacaedfb11d653877d1eb430d013f55470363
TheAntTeam <compact.pcb.maker.team@gmail.com>
2021-03-20 22:13:43 +0100

    Moved all UI interface connections to UI Manager.

--- app_main.py
+++ app_main.py
@@ -1,13 +1,12 @@
 import sys
 from PySide2.QtWidgets import QMainWindow, QApplication
-from PySide2.QtCore import Signal, QThread
+from PySide2.QtCore import QThread
 from queue import Queue
 from ui_newCNC import Ui_MainWindow  # convert like this: pyside2-uic newCNC.ui > ui_newCNC.py
 """ Custom imports """
 from serial_manager import SerialWorker
 from controller_manager import ControllerWorker
 from style_manager import StyleManager
-from shape_core.visual_manager import VisualLayer
 from ui_manager import UiManager
 
 
@@ -16,20 +15,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__()
-        self.connection_status = False
-        self.last_open_dir = "."
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-
-        self.vl = VisualLayer(self.ui.viewCanvasWidget)
-        self.ui.refreshButton.clicked.connect(self.handle_refresh_button)
-        self.ui.connectButton.clicked.connect(self.handle_connect_button)
-        self.ui.clearTerminalButton.clicked.connect(self.handle_clear_terminal)
-        self.ui.send_text_edit.setPlaceholderText('input here')
-        self.ui.send_text_edit.hide()
-        self.ui.send_push_button.hide()
-        self.ui.actionHide_Show_Console.triggered.connect(self.hide_show_console)
 
         # Control Worker Thread, started as soon as the thread pool is started.
         self.control_thread = QThread(self)
@@ -52,52 +40,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.serial_thread.quit()
         self.control_thread.quit()
         app.exit(0)
-
-    def handle_refresh_button(self):
-        """Get list of serial ports available."""
-        ls = self.serialWo.get_port_list()
-        if ls:
-            # print(ls)
-            # self.ui.textEdit.append("Listing serial ports: ")
-            # self.ui.textEdit.append(str(ls))
-            self.ui.serialPortsComboBox.clear()
-            self.ui.serialPortsComboBox.addItems(ls)
-        else:
-            # print('No serial ports available.')
-            self.ui.textEdit.append('No serial ports available.')
-            self.ui.serialPortsComboBox.clear()
-
-    def handle_connect_button(self):
-        """Connect/Disconnect button opens/closes the selected serial port and
-           creates the serial worker thread. If the thread was
-           already created previously and paused, it revives it."""
-        if not self.connection_status:
-            # print(self.serialPortsComboBox.currentText())
-            if self.serialWo.open_port(self.ui.serialPortsComboBox.currentText()):
-                self.connection_status = True
-                self.ui.connectButton.setText("Disconnect")
-                self.ui.serialPortsComboBox.hide()
-                self.ui.refreshButton.hide()
-                self.ui.send_text_edit.show()
-                self.ui.send_push_button.show()
-        else:
-            self.serialWo.close_port()
-            self.connection_status = False
-            self.ui.connectButton.setText("Connect")
-            self.ui.serialPortsComboBox.show()
-            self.ui.refreshButton.show()
-            self.ui.send_text_edit.hide()
-            self.ui.send_push_button.hide()
-            self.ui.statusLabel.setText("Not Connected")
-
-    def handle_clear_terminal(self):
-        self.ui.textEdit.clear()
-
-    def hide_show_console(self):
-        if self.ui.actionHide_Show_Console.isChecked():
-            self.ui.consoleTextEdit.show()
-        else:
-            self.ui.consoleTextEdit.hide()
 
 
 if __name__ == "__main__":
--- ui_manager.py
+++ ui_manager.py
@@ -19,15 +19,26 @@ class UiManager(QObject):
     def __init__(self, main_win, ui, control_worker, serial_worker):
         super(UiManager, self).__init__()
         self.last_open_dir = "."
+        self.connection_status = False
         self.main_win = main_win
         self.ui = ui
         self.controlWo = control_worker
         self.serialWo = serial_worker
         self.vis_layer = VisualLayer(self.ui.viewCanvasWidget)
         self.layer_colors = Od([(k, v) for k, v in zip(self.L_TAGS, self.COLORS)])
+        self.L_TEXT = [self.ui.topFileLineEdit, self.ui.bottomFileLineEdit,
+                       self.ui.profileFileLineEdit, self.ui.drillFileLineEdit]
+        self.layers_te = Od([(k, t) for k, t in zip(self.L_TAGS, self.L_TEXT)])
+
+        self.ui.send_text_edit.setPlaceholderText('input here')
+        self.ui.send_text_edit.hide()
+        self.ui.send_push_button.hide()
 
         # Connect Widgets signals and slots
         # From UI to UI Manager
+        self.ui.refreshButton.clicked.connect(self.handle_refresh_button)
+        self.ui.connectButton.clicked.connect(self.handle_connect_button)
+        self.ui.clearTerminalButton.clicked.connect(self.handle_clear_terminal)
         self.ui.send_push_button.clicked.connect(self.send_input)
         self.ui.send_text_edit.returnPressed.connect(self.send_input)
         self.ui.unlockButton.clicked.connect(self.handle_unlock)
@@ -35,6 +46,8 @@ class UiManager(QObject):
         self.ui.xMinusButton.clicked.connect(self.handle_x_minus)
         self.ui.tabWidget.currentChanged.connect(self.check_align_is_active)
         self.ui.verticalSlider.valueChanged.connect(self.update_threshold)
+        self.ui.actionHide_Show_Console.triggered.connect(self.hide_show_console)
+
         # From UI Manager to Controller
         self.align_active_s.connect(self.controlWo.set_align_is_active)
         self.load_layer_s.connect(self.controlWo.load_new_layer)
@@ -47,7 +60,6 @@ class UiManager(QObject):
             lambda: self.load_gerber_file(self.L_TAGS[2], "Load Profile Gerber File", "Gerber (*.gbr *.GBR)"))
         self.ui.drillLoadButton.clicked.connect(
             lambda: self.load_gerber_file(self.L_TAGS[3], "Load Drill Excellon File", "Excellon (*.xln *.XLN)"))
-
         self.ui.topViewCheckBox.stateChanged.connect(
             lambda: self.set_layer_visible("top", self.ui.topViewCheckBox.isChecked()))
         self.ui.bottomViewCheckBox.stateChanged.connect(
@@ -59,15 +71,19 @@ class UiManager(QObject):
 
         # From UI Manager to Serial Manager
         self.serial_send_s.connect(self.serialWo.send)
+
         # From Controller Manager to UI Manager
         self.controlWo.update_layer_s.connect(self.visualize_new_layer)
         self.controlWo.update_camera_image_s.connect(self.update_camera_image)
         self.controlWo.update_status_s.connect(self.update_status)
         self.controlWo.update_console_text_s.connect(self.update_console_text)
+
         # From Controller Manager to Serial Manager
         self.controlWo.serial_send_s.connect(self.serialWo.send)
+
         # From Serial Manager to UI Manager
         self.serialWo.update_console_text_s.connect(self.update_console_text)
+
         # From Serial Manager to Control Manager
         self.serialWo.rx_queue_not_empty_s.connect(self.controlWo.parse_rx_queue)
 
@@ -86,18 +102,7 @@ class UiManager(QObject):
     @Slot(Od, str, str, bool)
     def visualize_new_layer(self, loaded_layer, layer, layer_path, holes):
         self.vis_layer.add_layer(layer, loaded_layer[0], self.layer_colors[layer], holes)
-        self.set_layer_path(layer, layer_path)
-
-    @Slot(str, str)
-    def set_layer_path(self, layer, layer_path):
-        if layer == "top":  # todo: use a dictionary or something similar???
-            self.ui.topFileLineEdit.setText(layer_path)
-        elif layer == "bottom":
-            self.ui.bottomFileLineEdit.setText(layer_path)
-        elif layer == "profile":
-            self.ui.profileFileLineEdit.setText(layer_path)
-        elif layer == "drill":
-            self.ui.drillFileLineEdit.setText(layer_path)
+        self.layers_te[layer].setText(layer_path)
 
     @Slot(QPixmap)
     def update_camera_image(self, pixmap):
@@ -129,6 +134,52 @@ class UiManager(QObject):
         # self.serialTxQu.put(self.ui.send_text_edit.text() + "\n")
         self.serial_send_s.emit(self.ui.send_text_edit.text() + "\n")
         self.ui.send_text_edit.clear()
+
+    def handle_refresh_button(self):
+        """Get list of serial ports available."""
+        ls = self.serialWo.get_port_list()
+        if ls:
+            # print(ls)
+            # self.ui.textEdit.append("Listing serial ports: ")
+            # self.ui.textEdit.append(str(ls))
+            self.ui.serialPortsComboBox.clear()
+            self.ui.serialPortsComboBox.addItems(ls)
+        else:
+            # print('No serial ports available.')
+            self.ui.textEdit.append('No serial ports available.')
+            self.ui.serialPortsComboBox.clear()
+
+    def handle_connect_button(self):
+        """Connect/Disconnect button opens/closes the selected serial port and
+           creates the serial worker thread. If the thread was
+           already created previously and paused, it revives it."""
+        if not self.connection_status:
+            # print(self.serialPortsComboBox.currentText())
+            if self.serialWo.open_port(self.ui.serialPortsComboBox.currentText()):
+                self.connection_status = True
+                self.ui.connectButton.setText("Disconnect")
+                self.ui.serialPortsComboBox.hide()
+                self.ui.refreshButton.hide()
+                self.ui.send_text_edit.show()
+                self.ui.send_push_button.show()
+        else:
+            self.serialWo.close_port()
+            self.connection_status = False
+            self.ui.connectButton.setText("Connect")
+            self.ui.serialPortsComboBox.show()
+            self.ui.refreshButton.show()
+            self.ui.send_text_edit.hide()
+            self.ui.send_push_button.hide()
+            self.ui.statusLabel.setText("Not Connected")
+
+    def handle_clear_terminal(self):
+        self.ui.textEdit.clear()
+
+    def hide_show_console(self):
+        if self.ui.actionHide_Show_Console.isChecked():
+            self.ui.consoleTextEdit.show()
+        else:
+            self.ui.consoleTextEdit.hide()
 
     def handle_unlock(self):
         print("unlock")
