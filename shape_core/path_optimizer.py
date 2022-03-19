f8458745978910a31d6bbb96cdddde25e3221673
TheAntTeam <compact.pcb.maker.team@gmail.com>
2022-03-19 15:10:03 +0100

    Removed unused code.

--- shape_core/path_optimizer.py
+++ shape_core/path_optimizer.py
@@ -126,24 +126,6 @@ class Optimizer:
             matingpool.append(population[index])
         return matingpool
 
-        child = []
-        childP1 = []
-        childP2 = []
-
-        geneA = int(random.random() * len(parent1))
-        geneB = int(random.random() * len(parent1))
-
-        startGene = min(geneA, geneB)
-        endGene = max(geneA, geneB)
-
-        for i in range(startGene, endGene):
-            childP1.append(parent1[i])
-
-        childP2 = [item for item in parent2 if item not in childP1]
-
-        child = childP1 + childP2
-        return child
-
     def breed(self, parent1, parent2):
         child = []
         childP1 = []
