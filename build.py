0153ca347a6be596abd86030a853394d88baaff8
TheAntTeam <compact.pcb.maker.team@gmail.com>
2022-03-23 20:37:13 +0100

    Added clean and all operations and updated comments.

--- build.py
+++ build.py
@@ -3,20 +3,33 @@ import argparse
 
 
 def install_requirements():
+    """Install required python packages."""
     os.system("pip3 install -r requirements.txt")
 
 
 def build_ui():
+    """Convert UI file to equivalent python code."""
     os.system("pyside2-uic the_ant_farm.ui > ui_the_ant_farm.py")
 
 
+def clean_ui():
+    """Remove python file generated from UI file."""
+    os.remove("ui_the_ant_farm.py")
+
+
 def build_resources():
+    """Convert QRC resource file to python code."""
     os.system("pyside2-rcc app_resources.qrc -o app_resources_rc.py")
+
+
+def clean_resources():
+    """Remove python file generated from QRC resource file."""
+    os.remove("app_resources_rc.py")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Build artifacts for The Ant Farm application.")
-    parser.add_argument("operation", choices=["req", "REQ", "ui", "UI", "res", "RES"])
+    parser.add_argument("operation", choices=["req", "REQ", "ui", "UI", "res", "RES", "clean", "CLEAN", "all", "ALL"])
 
     parsed_args = parser.parse_args()
 
@@ -27,3 +40,10 @@ if __name__ == "__main__":
         build_ui()
     elif operation == "res":
         build_resources()
+    elif operation == "all":
+        install_requirements()
+        build_ui()
+        build_resources()
+    elif operation == "clean":
+        clean_ui()
+        clean_resources()
