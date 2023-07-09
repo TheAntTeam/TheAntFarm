2d4431746ad06f7188a79fa389532c0a31f5152e
KLabsForMakers <mattia.monetti@gmail.com>
2023-07-09 14:06:15 +0200

    Updated the path_optimizer which optimizes the drill path. Partially vectorized the genetic algorithm (x3 in speed). Added two other algorithms, developed with the help of ChatGPT and then optimized. The first is the "nearest_insertion" (chosen now as default) fast but far from the optimal, the second "two_opt", slow but which is close to the optimal path. Integration into the GUI will follow.

--- src/TheAntFarm/shape_core/path_optimizer.py
+++ src/TheAntFarm/shape_core/path_optimizer.py
@@ -1,110 +1,156 @@
 # from: https://github.com/ezstoltz/genetic-algorithm
 # import numpy as np, random, operator, pandas as pd, matplotlib.pyplot as plt
+import sys
 import numpy as np
+from scipy.spatial import distance
+from operator import itemgetter
+from scipy.sparse.csgraph import shortest_path
 import random
 import operator
 from shapely.geometry import LineString, Point
 #import matplotlib.pyplot as plt
 
 
-class City:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    
-    def distance(self, city):
-        xDis = abs(self.x - city.x)
-        yDis = abs(self.y - city.y)
-        distance = np.sqrt((xDis ** 2) + (yDis ** 2))
-        return distance
+class Optimizer:
+    def __init__(self, coords, optimizer_type="nearest_insertion"):
+        self.v = len(coords)
+        self.coords = coords
+        self.distances = distance.cdist(coords, coords, 'euclidean')
+        self.optimizer_type = optimizer_type
 
-    def coords(self):
-        return (self.x, self.y)
+    def calculate_path_distance(self, path):
+        total_distance = np.sum(self.distances[path[:-1], path[1:]])
+        return total_distance
 
-    def __repr__(self):
-        return "(" + str(self.x) + "," + str(self.y) + ")"
+    def nearest_insertion(self, points):
+        num_points = len(points)
+        unvisited = set(range(num_points))
+
+        first_point_matrix = distance.cdist([(0., 0.)], points, 'euclidean')
+        first_point_index = np.argmin(first_point_matrix)
+
+        path = [first_point_index]
+        unvisited.remove(first_point_index)
+
+        while unvisited:
+            min_distance = float('inf')
+            nearest_point = None
+
+            for current_point in path:
+                for candidate in unvisited:
+                    d = self.distances[current_point, candidate]
+                    if d < min_distance:
+                        min_distance = d
+                        nearest_point = candidate
+                        next_point = current_point
+
+            insert_index = path.index(next_point) + 1
+            path.insert(insert_index, nearest_point)
+            unvisited.remove(nearest_point)
+
+        return path
+
+    def two_opt(self, path):
+        new_distance = self.calculate_path_distance(path)
+        points = self.coords
+        best_path = path
+        improved = True
+        while improved:
+            improved = False
+            for i in range(1, len(points) - 2):
+                for j in range(i + 1, len(points)):
+                    if j - i == 1:
+                        continue
+                    new_path = path[:]
+                    new_path[i:j] = path[i:j][::-1]
+                    new_distance = self.calculate_path_distance(new_path)
+                    if new_distance < self.calculate_path_distance(best_path):
+                        best_path = new_path
+                        improved = True
+            path = best_path
+        # print("Final Distance: ", new_distance)
+        return best_path
+
+    def get_optimized_path(self):
+        path_ids = list(range(len(self.coords)))
+
+        start_distance = self.calculate_path_distance(path_ids)
+        print("Initial Distance: ", start_distance)
+
+        if self.optimizer_type == "genetic":
+            path_ids = self.nearest_insertion(self.coords)
+            go = GeneticOptimizer(self.coords)
+            path_ids = go.get_optimized_path(cityList=path_ids)
+
+        if self.optimizer_type == "two_opt":
+            path_ids = self.two_opt(path_ids)
+
+        if self.optimizer_type == "nearest_insertion":
+            path_ids = self.nearest_insertion(self.coords)
+
+        final_distance = self.calculate_path_distance(path_ids)
+        print("Final Distance: ", final_distance)
+
+        return np.array(self.coords)[path_ids].tolist()
+
+
+class Cities:
+
+    def __init__(self, coords):
+        self.coords = coords
+        self.distances = distance.cdist(coords, coords, 'euclidean')
+
+    def length(self, path0, path1):
+        return np.sum(self.distances[path0, path1])
 
 
 class Fitness:
-    def __init__(self, route):
+    def __init__(self, route, cities):
         self.route = route
+        self.cities = cities
         self.distance = 0
         self.fitness = 0.0
-    
+
     def routeDistance(self):
         if self.distance == 0:
-            pathDistance = 0
-            for i in range(0, len(self.route)):
-                fromCity = self.route[i]
-                toCity = None
-                if i + 1 < len(self.route):
-                    toCity = self.route[i + 1]
-                else:
-                    toCity = self.route[0]
-                pathDistance += fromCity.distance(toCity)
-            self.distance = pathDistance
+            route0 = self.route
+            route1 = np.roll(self.route, -1)
+            self.distance = self.cities.length(route0, route1)
         return self.distance
-    
+
     def routeFitness(self):
         if self.fitness == 0:
             self.fitness = 1 / float(self.routeDistance())
         return self.fitness
 
 
-class Optimizer:
+class GeneticOptimizer:
 
     def __init__(self, points_coord):
         self.points_coord = points_coord
         self.population = []
+        self.cities = Cities(points_coord)
 
     @staticmethod
     def createRoute(cityList):
-        route = random.sample(cityList, len(cityList))
+        route = cityList.copy()
+        random.shuffle(route)
         return route
 
     def initialPopulation(self, popSize, cityList):
-        population = []
-        for i in range(0, popSize):
+        population = [cityList]
+        for i in range(0, popSize-1):
             population.append(self.createRoute(cityList))
-        return population
-
-    @staticmethod
-    def check_pop_intersection(population):
-        if len(population):
-            routes = []
-            c = population[0]
-            pp = c.coords()
-            nc = None
-            for i in range(1, len(population)):
-                nc = population[i]
-                p = nc.coords()
-                routes.append(LineString([pp, p]))
-                pp = p[:]
-            if nc is not None:
-                routes.append(LineString([pp, c.coords()]))
-
-            flags = [False for z in range(len(routes))]
-            step = 1
-            for i in range(len(routes)):
-                for j in range(i + 2, len(routes) - step):
-                    a = routes[i]
-                    b = routes[j]
-                    if a.intersects(b):
-                        flags[i] = True
-                        flags[j] = True
-                step = 0
-            return flags
-        else:
-            return [False for z in range(len(population))]
+        return np.array(population)
 
     def rankRoutes(self, population):
         fitnessResults = {}
         for i in range(0, len(population)):
-            fitnessResults[i] = Fitness(population[i]).routeFitness()
+            # print("Population:", population[i])
+            fitnessResults[i] = Fitness(population[i], self.cities).routeFitness()
         return sorted(fitnessResults.items(), key=operator.itemgetter(1), reverse=True)
 
     def selection(self, popRanked, eliteSize):
-
         selectionResults = []
         n = np.array(popRanked)
         cs = n[1, :].cumsum()
@@ -120,10 +166,7 @@ class Optimizer:
         return selectionResults
 
     def matingPool(self, population, selectionResults):
-        matingpool = []
-        for i in range(0, len(selectionResults)):
-            index = selectionResults[i]
-            matingpool.append(population[index])
+        matingpool = population[selectionResults]
         return matingpool
 
     def breed(self, parent1, parent2):
@@ -148,15 +191,14 @@ class Optimizer:
     def breedPopulation(self, matingpool, eliteSize):
         children = []
         length = len(matingpool) - eliteSize
-        pool = random.sample(matingpool, len(matingpool))
-
-        for i in range(0, eliteSize):
-            children.append(matingpool[i])
+        pool = matingpool.copy()
+        random.shuffle(matingpool)
+        children = matingpool[range(0, eliteSize)].tolist()
 
         for i in range(0, length):
             child = self.breed(pool[i], pool[len(matingpool)-i-1])
             children.append(child)
-        return children
+        return np.array(children)
 
     def mutate(self, individual, mutationRate):
         for swapped in range(len(individual)):
@@ -176,7 +218,7 @@ class Optimizer:
         for ind in range(0, len(population)):
             mutatedInd = self.mutate(population[ind], mutationRate)
             mutatedPop.append(mutatedInd)
-        return mutatedPop
+        return np.array(mutatedPop)
 
     def nextGeneration(self, currentGen, eliteSize, mutationRate):
         popRanked = self.rankRoutes(currentGen)
@@ -188,7 +230,7 @@ class Optimizer:
 
     def geneticAlgorithm(self, population, popSize, eliteSize, mutationRate, generations):
         pop = self.initialPopulation(popSize, population)
-        print("Initial distance: " + str(1 / self.rankRoutes(pop)[0][1]))
+        # print("Initial distance: " + str(1 / self.rankRoutes(pop)[0][1]))
 
         x = int(generations/10)
         c = 0
@@ -202,7 +244,7 @@ class Optimizer:
             else:
                 c += 1
 
-        print("Final distance: " + str(1 / self.rankRoutes(pop)[0][1]))
+        # print("Final distance: " + str(1 / self.rankRoutes(pop)[0][1]))
         bestRouteIndex = self.rankRoutes(pop)[0][0]
         bestRoute = pop[bestRouteIndex]
         return bestRoute
@@ -211,49 +253,18 @@ class Optimizer:
     def rotate(l, n):
         return l[n:] + l[:n]
 
-    def get_optimized_path(self):
-        points_coord = self.points_coord
-        cityList = []
-        for p in points_coord:
-            cityList.append(City(x=p[0], y=p[1]))
+    def get_optimized_path(self, cityList=None):
+        points_coord = np.array(self.points_coord)
+        if cityList is None:
+            cityList = np.array(range(0, len(points_coord)))
         bestRoute = self.geneticAlgorithm(population=cityList, popSize=400, eliteSize=50, mutationRate=0.02, generations=800)
-        optimized_coords = []
 
-        for c in bestRoute:
-            optimized_coords.append((c.x, c.y))
+        first_point_matrix = distance.cdist([(0., 0.)], points_coord[bestRoute], 'euclidean')
+        first_point_index = np.argmin(first_point_matrix)
+        # optimized_coords = np.roll(points_coord[bestRoute], -first_point_index)
+        optimized_ids = np.roll(bestRoute, -first_point_index)
 
-        # the path is ordered so that it starts from the point closest to 0.0
-
-        min_id = 0
-        min_d = Point(optimized_coords[0]).distance(Point((0.0, 0.0)))
-        for i in range(len(optimized_coords)):
-            d = Point(optimized_coords[i]).distance(Point((0.0, 0.0)))
-            if d < min_d:
-                min_id = i
-                min_d = d
-
-        pre_id = min_id - 1
-        post_id = min_id + 1
-        if min_id == len(optimized_coords) - 1:
-            post_id = 0
-
-        pre_d = Point(optimized_coords[pre_id]).distance(Point((0.0, 0.0)))
-        post_d = Point(optimized_coords[post_id]).distance(Point((0.0, 0.0)))
-
-        if pre_d < post_d:
-            noc = self.rotate(optimized_coords, min_id)
-        else:
-            noc = self.rotate(optimized_coords, post_id)
-
-        optimized_coords = noc
-
-        x = []
-        y = []
-        for c in optimized_coords:
-            x.append(c[0])
-            y.append(c[1])
-
-        return optimized_coords
+        return optimized_ids
 
 
 if __name__ == "__main__":
