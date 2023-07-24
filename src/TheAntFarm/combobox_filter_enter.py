53c9aea03d57566a8ef2c66a673f793fb1a2e82c
TheAntTeam <compact.pcb.maker.team@gmail.com>
2023-07-24 23:42:22 +0200

    Adding missing file of custom combobox that filters and redirects Enter keys press events.

---
+++ src/TheAntFarm/combobox_filter_enter.py
@@ -0,0 +1,15 @@
+from PySide2.QtWidgets import QComboBox
+from PySide2 import QtCore
+
+
+class ComboBoxFilterEnter(QComboBox):
+    """Custom combobox class that handles Enter key events."""
+
+    key_event_cb = QtCore.Signal(int, name='key_event')
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_Enter or event.key() == QtCore.Qt.Key_Return:
+            self.key_event_cb.emit(event.key())
+            event.accept()
+        else:
+            super(ComboBoxFilterEnter, self).keyPressEvent(event)
