101f5e62622dfed670dd7a2615cec6a8a9074b82
TheAntTeam <compact.pcb.maker.team@gmail.com>
2024-07-21 00:01:36 +0200

    Adding wheel event on camera label class and connected signal to combobox of zoom.

---
+++ src/TheAntFarm/qcamera_label.py
@@ -0,0 +1,18 @@
+from PySide2.QtWidgets import QLabel
+from PySide2.QtGui import QWheelEvent
+from PySide2.QtCore import Signal
+
+
+class QCameraLabel(QLabel):
+    mouse_wheel_up_or_down_s = Signal(int)
+
+    def __init__(self, parent):
+        super(QCameraLabel, self).__init__(parent)
+
+    def wheelEvent(self, event: QWheelEvent):
+        if event.angleDelta().x() > 0 or event.angleDelta().y() > 0:
+            self.mouse_wheel_up_or_down_s.emit(1)
+        elif event.angleDelta().x() < 0 or event.angleDelta().y() < 0:
+            self.mouse_wheel_up_or_down_s.emit(-1)
+        else:
+            pass  # todo: this event should never be catched, an error could be raised
