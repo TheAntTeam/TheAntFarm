d3fe0fb3e4b9cf5335357881f08650e27cbf1994
TheAntTeam <compact.pcb.maker.team@gmail.com>
2025-08-22 23:32:06 +0200

    Corrected calls parameters in the GridLayout addWidget call for updated API in pyside 6 vs 2

--- src/TheAntFarm/ui_manager/ui_about.py
+++ src/TheAntFarm/ui_manager/ui_about.py
@@ -6,6 +6,7 @@ from PySide6.QtGui import QPixmap
 class UiAbout(QObject):
     """Manage UI objects, signals and slots"""
     def __init__(self, main_win, app_settings):
+        super().__init__()
         self.main_win = main_win
         self.app_settings = app_settings
 
@@ -22,45 +23,45 @@ class UiAbout(QObject):
         ico_label.setPixmap(qp_map)
         ico_label.setFixedSize(QSize(100, 100))
         ico_label.setScaledContents(True)
-        grid.addWidget(ico_label, 0, 0, alignment=Qt.AlignCenter, rowSpan=1, columnSpan=1)
+        grid.addWidget(ico_label, 0, 0, 1, 1, alignment=Qt.AlignCenter)
 
         version_la = QLabel("Version " + self.app_settings.app_version, about_dlg)
         version_la.setStyleSheet("QLabel{font-size: 16pt;}")
-        grid.addWidget(version_la, 0, 1, alignment=Qt.AlignCenter, rowSpan=1, columnSpan=1)
+        grid.addWidget(version_la, 0, 1, 1, 1, alignment=Qt.AlignCenter)
 
         credits_la = QLabel("Developed by: ", about_dlg)
         credits_la.setStyleSheet("QLabel{font-size: 16pt;}")
-        grid.addWidget(credits_la, 1, 0, alignment=Qt.AlignCenter | Qt.AlignVCenter, rowSpan=1, columnSpan=1)
+        grid.addWidget(credits_la, 1, 0, 1, 1, alignment=Qt.AlignCenter | Qt.AlignVCenter)
 
         credits_2_la = QLabel("The Ant Team and \nthe kind contributors", about_dlg)
         credits_2_la.setStyleSheet("QLabel{font-size: 16pt;}")
         credits_2_la.setWordWrap(True)
-        grid.addWidget(credits_2_la, 1, 1, alignment=Qt.AlignCenter | Qt.AlignVCenter, rowSpan=1, columnSpan=1)
+        grid.addWidget(credits_2_la, 1, 1, 1, 1, alignment=Qt.AlignCenter | Qt.AlignVCenter)
 
         license_la = QLabel("License: ", about_dlg)
         license_la.setStyleSheet("QLabel{font-size: 16pt;}")
-        grid.addWidget(license_la, 2, 0, alignment=Qt.AlignCenter | Qt.AlignVCenter, rowSpan=1, columnSpan=1)
+        grid.addWidget(license_la, 2, 0, 1, 1, alignment=Qt.AlignCenter | Qt.AlignVCenter)
 
         license_2_la = QLabel("<a href=\"https://github.com/TheAntTeam/TheAntFarm/blob/main/LICENSE\">MIT License</a>", about_dlg)
         license_2_la.setStyleSheet("QLabel{font-size: 16pt;}")
         license_2_la.setTextFormat(Qt.RichText)
         license_2_la.setTextInteractionFlags(Qt.TextBrowserInteraction)
         license_2_la.setOpenExternalLinks(True)
-        grid.addWidget(license_2_la, 2, 1, alignment=Qt.AlignCenter | Qt.AlignVCenter, rowSpan=1, columnSpan=1)
+        grid.addWidget(license_2_la, 2, 1, 1, 1, alignment=Qt.AlignCenter | Qt.AlignVCenter)
 
         yt_la = QLabel("<a href=\"https://www.youtube.com/c/TheAntPCBMaker\">Youtube Here!</a>", about_dlg)
         yt_la.setStyleSheet("QLabel{font-size: 16pt;}")
         yt_la.setTextFormat(Qt.RichText)
         yt_la.setTextInteractionFlags(Qt.TextBrowserInteraction)
         yt_la.setOpenExternalLinks(True)
-        grid.addWidget(yt_la, 3, 0, alignment=Qt.AlignCenter | Qt.AlignVCenter, rowSpan=1, columnSpan=1)
+        grid.addWidget(yt_la, 3, 0, 1, 1, alignment=Qt.AlignCenter | Qt.AlignVCenter)
 
         repo_la = QLabel("<a href=\"https://github.com/TheAntTeam/TheAntFarm\">Repo Here!</a>", about_dlg)
         repo_la.setStyleSheet("QLabel{font-size: 16pt;}")
         repo_la.setTextFormat(Qt.RichText)
         repo_la.setTextInteractionFlags(Qt.TextBrowserInteraction)
         repo_la.setOpenExternalLinks(True)
-        grid.addWidget(repo_la, 3, 1, alignment=Qt.AlignCenter | Qt.AlignVCenter, rowSpan=1, columnSpan=1)
+        grid.addWidget(repo_la, 3, 1, 1, 1, alignment=Qt.AlignCenter | Qt.AlignVCenter)
 
         about_dlg.show()
 
